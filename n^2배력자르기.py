2f7b1270f9978a0f3b9313fb7840530aaa18d65a
lmh <tnwtw@naver.com>
2023-10-04 11:52:14 +0900

    edit code method

---
+++ n^2배력자르기.py
@@ -0,0 +1,7 @@
+def solution(n, left, right):
+    answer = []
+    for nn in range(n):
+        for j in range(n): 
+            answer.append(max(nn,j)+1)
+    return answer[left:right+1]
+solution(3,2,5)
